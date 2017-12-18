c00b3b0e9ee92a9d623b63f25db0ed211dad0852
Funtaztic <abelszabolcsi@gmail.com>
2017-12-18 08:59:07 +0100

    PROTECTIVE_GEAR = SHIELD + HELMET

--- Maximum_Complexity.py
+++ Maximum_Complexity.py
@@ -15,9 +15,24 @@ print ('hello world')
 
 #This will be a basic TRPG. Amazing adventures and exciting experiences.
 
+##############################################################################
+
 ###VARIABLES###
 
 ##BASICS##
 HP = 100
 MONEY = 0
 ##BASICS##
+
+#WEAPONS AND GEAR#
+    #For example: 0=no weapon, 10=weak weapon, 200=strong weapon
+SWORD = 0
+
+PROTECTIVE_GEAR = SHIELD + HELMET
+SHIELD = 0
+HELMET = 0
+
+#WEAPONS AND GEAR#
+
+
+##############################################################################
